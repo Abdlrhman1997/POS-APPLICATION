bbfde1b901751db52a6c7cfd8382a21bfe26d260
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-08-08 15:59:03 -0700

    Add drink.validation.js

---
+++ src/modules/drink/drink.validation.js
@@ -0,0 +1,12 @@
+import joi from "joi";
+
+export const updateDrink = {
+  body: joi
+    .object({
+      name: joi.string().min(3).max(25).required(),
+      price: joi.number().integer().min(5).max(120).required(),
+      image: joi.string(),
+      // .required(),
+    })
+    .required(),
+};
